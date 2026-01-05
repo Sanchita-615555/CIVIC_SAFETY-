a3d9eaac78451aae39dbe2a9b1d33940aee31897
Sanchita Sinha <sanchitasinha628@gmail.com>
2026-01-05 15:13:34 +0530

    Update landing page design

--- safety/src/components/LandingSection.jsx
+++ safety/src/components/LandingSection.jsx
@@ -5,9 +5,10 @@ function LandingSection() {
   return (
     <section
       id="home"
-      className="w-full h-screen flex flex-row items-center
+      className="w-full min-h-screen
+                 flex flex-row items-center
                  bg-gradient-to-b from-blue-100 via-blue-50 to-white
-                 relative pt-8 px-3"
+                 relative px-6 pt-20"
     >
       {/* NAVBAR */}
       <div className="w-full absolute top-0 left-0 z-50">
@@ -15,30 +16,32 @@ function LandingSection() {
       </div>
 
       {/* LEFT SIDE: TEXT */}
-      <div className="w-1/2 flex flex-col justify-center pl-2">
-     <h1 className="text-3xl font-bold text-gray-900 mb-1">
-  CIVIC SAFETY
-</h1>
+      <div className="w-1/2 flex flex-col justify-start">
+        <h1 className="text-3xl font-bold text-gray-900 mb-1">
+          CIVIC SAFETY
+        </h1>
 
-<h2 className="text-2xl font-semibold text-blue-900 mb-3 ml-8">
-  OUR PRIORITY
-</h2>
- <p className="text-sm text-gray-700 max-w-sm leading-relaxed">
+        <h2 className="text-2xl font-semibold text-blue-900 mb-3 ml-6">
+          OUR PRIORITY
+        </h2>
+
+        <p className="text-sm text-gray-700 max-w-sm leading-relaxed">
           Ensuring a secure community for everyone through awareness,
           prevention, and collective responsibility. Together we work
-          towards a safer and more prepared society. 
+          towards a safer and more prepared society.
         </p>
       </div>
-{/* RIGHT SIDE: IMAGE */}
-<div className="w-1/2 flex justify-end items-center pr-5 mt-8">
-  <img
-    src="/landing.jpg"
-    alt="Civic Safety"
-    className="w-64 h-50 object-cover rounded-lg shadow-md translate-x-5"
-  />
-</div>
-  </section>
+
+      {/* RIGHT SIDE: IMAGE */}
+      <div className="w-1/2 flex justify-end items-start">
+        <img
+          src="/landing.jpg"
+          alt="Civic Safety"
+          className="w-60 h-50 *:**:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          object-cover rounded-lg shadow-md"
+        />
+      </div>
+    </section>
   );
-}
+}                                                     
 
 export default LandingSection;
